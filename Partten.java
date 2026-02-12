753dfe496b453559e4f18b22f857c79a2820f7e9
joshi-samridhi <samridhijoshi03@gmail.com>
2026-02-12 22:03:48 +0530

    Add files via upload

---
+++ Partten.java
@@ -0,0 +1,43 @@
+// import java.util.*;
+
+public class Partten {
+  
+public static void main()
+{
+//   Scanner s = new Scanner(System.in);
+//   System.out.println("Enter number");
+
+//   int n= s.nextInt();
+
+  //  Partten 1
+
+//   for(int i =0; i<4; i++)
+//   {
+//     for(int j=1; j<=5; j++)
+//    {
+//     System.out.print("*");
+//    }
+// System.out.println("*");
+//   }
+ 
+//  Partten 2
+  
+for(int i=1; i<=4; i++)
+{
+  for(int j=4; j>=i; j--)
+  {
+    System.out.print("");
+  }
+System.out.println("*");  
+}
+
+}    
+}
+
+// for(int i=4; i>=1; i--)
+// {
+//   for(int j=1; j<=i; j++)
+//   {
+//     System.out.print("*");
+//   }
+// System.out.println("*"); 
